3fab777341f88c71063a8e670f64c682bc1fd9f5
Nathan Anderson <nallenanderson@gmail.com>
2022-08-30 11:07:02 -0400

    Added stupid routing

--- routes/basic.js
+++ routes/basic.js
@@ -7,6 +7,21 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   res.status(200).send({ message: 'You did it!' });
-}
+});
+
+router.get('/users', (req, res) => {
+  res.status(200).send({
+    users: [
+      {
+        id: 'gui123abc',
+        name: 'Some Daniel'
+      },
+      {
+        id: 'abc321iug',
+        name: 'Tottus McFarlan'
+      },
+    ]
+  })
+});
 
 module.exports = router;
